9ca8768f96ad10a25add51d61d11e2a8fb07c149
Misil4 <carlossilvamarques.cs@gmail.com>
2022-04-25 20:33:43 +0200

    2

--- assets/urls.js
+++ assets/urls.js
@@ -1,5 +1,5 @@
 export const urls = {url: "https://jkanime.net/",
-  animeUrl : "https://jkanime.net/",
+  animeUrl : "https://www3.animeflv.net/anime/",
   searchUrl: "https://jkanime.net/buscar/",
   watchUrl: "https://jkanime.net/",
   downloadUrl: "https://monoschinos2.com/ver/"}
