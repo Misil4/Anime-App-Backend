544ab482c0d0dee00ae3055900dd3bb4562db0cb
csilvamarq <carlossilvamarques.cs@gmail.com>
2023-01-07 23:38:35 +0100

    (UPDATE) solved bug on seasonalAnime

--- controller/SeasonalAnime.js
+++ controller/SeasonalAnime.js
@@ -4,13 +4,14 @@ import chromium from "chrome-aws-lambda";
 import { urls } from "../assets/urls.js";
 import cheerio from "cheerio";
 export const getlatestAnimeAdded = async (req, res) => {
-  const browser = await puppeteer.launch({args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
-  defaultViewport: chromium.defaultViewport,
-  executablePath: await chromium.executablePath,
-  headless: true,
-  ignoreHTTPSErrors: true, });
-  const page = await browser.newPage();
-  await page.goto(urls.url);
+  const browser = await chromium.puppeteer.launch({args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    headless: true,
+    ignoreHTTPSErrors: true, });
+    const page = await browser.newPage();
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36');
+    await page.goto(urls.url);
   let anilist = []
   const data = await page.evaluate(() => document.querySelector('*').outerHTML);
   const $ = cheerio.load(data)
