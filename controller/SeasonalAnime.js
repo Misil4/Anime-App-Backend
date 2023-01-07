55f94f4cc52dd6faada42b34386b20de10b72934
csilvamarq <carlossilvamarques.cs@gmail.com>
2023-01-08 00:03:44 +0100

    (UPDATE) testing

--- controller/SeasonalAnime.js
+++ controller/SeasonalAnime.js
@@ -13,6 +13,7 @@ export const getlatestAnimeAdded = async (req, res) => {
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36');
     await page.goto(urls.url);
   let anilist = []
+  console.log(page)
   const data = await page.evaluate(() => document.querySelector('*').outerHTML);
   const $ = cheerio.load(data)
   const test = $('.maximoaltura').find('.bloqq').each((index, value) => anilist.push({ image: value.children[0].children[1].children[1].attribs.src, name: value.children[0].children[3].children[1].children[0].data, episode: parseInt(value.children[0].children[3].children[3].children[0].data.substring(136, 137)), url: value.attribs.href }))
