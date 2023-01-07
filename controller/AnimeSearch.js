23e02b1bc551e26306ec57872b96739e785bf368
csilvamarq <carlossilvamarques.cs@gmail.com>
2023-01-08 00:57:23 +0100

    (UPDATE) testing headers

--- controller/AnimeSearch.js
+++ controller/AnimeSearch.js
@@ -11,7 +11,14 @@ export const searchAnime = async (req,res) => {
     headless: true,
     ignoreHTTPSErrors: true, });
     const page = await browser.newPage();
-    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36');
+    await page.setExtraHTTPHeaders({ 
+		'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36', 
+		'upgrade-insecure-requests': '1', 
+		'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8', 
+		'accept-encoding': 'gzip, deflate, br', 
+		'accept-language': 'en-US,en;q=0.9,en;q=0.8' 
+	}); 
+    await page.waitForTimeout((Math.floor(Math.random() * 12) + 5) * 1000) 
     await page.goto(url);
     const results = [];
     const data = await page.evaluate(() => document.querySelector('*').outerHTML);
