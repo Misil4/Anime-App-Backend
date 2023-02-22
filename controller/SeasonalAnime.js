04fbd1bb327eee36c129ce29295b8a08d7034d20
csilvamarq <csilvamarq@ceit.es>
2023-02-22 12:57:39 +0100

    (UPDATE) delete waitselector

--- controller/SeasonalAnime.js
+++ controller/SeasonalAnime.js
@@ -10,7 +10,6 @@ export const getlatestAnimeAdded = async (req, res) => {
     const page = await browser.newPage();
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36');
     await page.goto(urls.url, { waitUntil: 'domcontentloaded' });
-    await page.waitForSelector(".maximoaltura")
   let anilist = []
   const data = await page.evaluate(() => document.querySelector('*').outerHTML);
   const $ = cheerio.load(data)
