e853345fddbb1d9845081128e6a7ca1ec7f0e93f
csilvamarq <csilvamarq@ceit.es>
2023-04-03 12:29:16 +0200

    (UPDATE) migrate seasonal anime to monoschinos

--- controller/SeasonalAnime.js
+++ controller/SeasonalAnime.js
@@ -8,7 +8,8 @@ export const getlatestAnimeAdded = async (req, res) => {
   const data = await humanoid.get(urls.url); 
   const $ = cheerio.load(data.body)
   console.log(anilist)
-  const test = $('.maximoaltura').find('.bloqq').each((index, value) => anilist.push({ image: value.children[0].children[1].children[1].attribs.src, name: value.children[0].children[3].children[1].children[0].data, episode: parseInt(value.children[0].children[3].children[3].children[0].data.replace(/\D/g, "")), url: value.attribs.href }))
+
+  const test = $('.row.row-cols-5').find('.col.col-md-6.col-lg-2.col-6').each((index, value) => anilist.push({ image: value.children[1].children[1].children[1].children[1].attribs['data-src'], name: value.children[1].attribs.title, episode: parseInt(value.children[1].children[1].children[1].children[3].children[1].children[1].children[0].data), url: value.children[1].attribs.href }))
   res.send(anilist)
 };
 export const getlatestSeriesAdded = async (req,res) => {
